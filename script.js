34518e50738adf263a57ad4bf77e36888c0e6ef3
khakhalnikitha <nikithakhakhal@gmail.com>
2026-04-03 13:42:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const truths = [
+  "You're not tired. You're distracted.",
+  "You know what to do. You just don't do it.",
+  "Excuses are your comfort zone.",
+  "Discipline is missing from your life."
+];
 
+function generateTruth() {
+  const random = Math.floor(Math.random() * truths.length);
+  document.getElementById("truth").innerText = truths[random];
+}
