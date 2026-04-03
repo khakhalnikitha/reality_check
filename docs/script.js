3e6fec52cb29fcd59c5dfd5e5f66d899bb2b5591
khakhalnikitha <nikithakhakhal@gmail.com>
2026-04-03 14:13:31 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -9,3 +9,14 @@ function generateTruth() {
   const random = Math.floor(Math.random() * truths.length);
   document.getElementById("truth").innerText = truths[random];
 }
+function answer(choice) {
+  let resultText = "";
+
+  if (choice === "yes") {
+    resultText = "You're disciplined. But are you consistent every day?";
+  } else {
+    resultText = "You lack discipline. Comfort controls you.";
+  }
+
+  document.getElementById("result").innerText = resultText;
+}
